cba71ead06bae4da020c567c2563fff455b90b2f
acvn <aldisatria1996@gmail.com>
2021-08-25 18:35:13 +0700

    asd

---
+++ hitung.php
@@ -0,0 +1,30 @@
+<?php
+
+function LabelBMI($nilai) {
+	if ($nilai <= 18.5 ) {
+	echo "underweight";
+} elseif ($nilai > 18.5 AND $nilai < 25 ) {
+	echo "normal";
+} else {
+	echo "overweight";
+}
+}
+
+
+function nilaiBMI($nilaiberat, $nilaitinggi) {
+	$BMI = $nilaiberat / pow($nilaitinggi,1);
+	$BMI = round($BMI,3);
+	echo "BMI anda adalah " . $BMI . "<br>" ;
+}
+
+
+$berat = $_GET["beratbadan"];
+$tinggi = $_GET["tinggibadan"] / 100;
+
+
+
+nilaiBMI($berat, $tinggi);
+LabelBMI($BMI);
+
+
+?>
