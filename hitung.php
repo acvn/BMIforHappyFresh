750c5e2d3c0095a8b06d3cb467af01232452b4c0
acvn <aldisatria1996@gmail.com>
2021-09-11 18:24:56 +0700

    asd

--- hitung.php
+++ hitung.php
@@ -1,33 +1,5 @@
 <?php
 
-
-// fungsi output nilai BMI
-function nilaiBMI($nilaiberat, $nilaitinggi) {
-	global $BMI;
-	$BMI = $nilaiberat / pow($nilaitinggi,2);
-	$BMI = round($BMI,1);
-	return $BMI;
-}
-
-
-//fungsi output label BMI
-function LabelBMI($nilai) {
-	if ($nilai >= 1 AND $nilai <= 18.5 ) {
-		$xx = "underweight";
-		return $xx;
-	} elseif ($nilai > 18.5 AND $nilai < 25 ) {
-		$xx = "normal";
-		return $xx;
-	} elseif ($nilai >= 25) {
-		$xx = "overweight";
-		return $xx;
-	} else {
-		$xx = "input salah";
-		return $xx;
-	}
-}
-
-
 //validasi input
 function validasi($data) {
 	$data = trim($data);
@@ -36,53 +8,75 @@ function validasi($data) {
 	return $data;
 }
 
+//hitung nilai bmi
+function nilaibmi($C, $D) {
+	$D = $D/100;
+	$AA = $C/pow($D, 2);
+	$AA = round($AA, 1);
+	return $AA;
+}
 
-//Kirim data
-function kirimdata($j, $k){
-	$servername = "localhost";
+//menentukan status bmi
+function statusbmi($E) {
+	if ($E >= 1 AND $E <= 18.5 ) {
+		$FF = "underweight";
+	} elseif ($E > 18.5 AND $E < 25 ) {
+		$FF = "normal";
+	} elseif ($E >= 25) {
+		$FF = "overweight";
+	} else {
+  $FF = "input salah";
+	}
+	return $FF;
+}
+
+//kirim data
+function kirimdata($J, $K){
+	$servername = "db";
 	$username = "aldi";
 	$password = "J4k4rt4!";
 	$database = "BMI";
-	$conn = mysqli_connect($servername,$username,$password,$database);
+	
+	$conn = mysqli_connect($servername, $username, $password, $database);
 	if(!$conn) {
 		die("connection failed: " . mysqli_connect_error());
 	}
-	$sql = "INSERT INTO databmi (nilaibmi, statusbmi) VALUES (" . $j .  ", '" . $k . "')";
-	if ($conn->query($sql)) {
-		$msg = array("bmi" => $j , "label" => "$k");
+	
+	$sql = "INSERT INTO databmi (nilaibmi, statusbmi) VALUES (" . $J .  ", '" . $K . "')";
+	if (mysqli_query($conn, $sql)) {
+		//echo 'Data sudah ditambahkan';
+		$msg = array("bmi" => $J , "label" => "$K" );
 		$json = $msg;
-		header('Content-type: application/json');
+		header('Content-type: application/json');		
 		echo json_encode($json);
-	}
+	} 
+	mysqli_close($conn);
 }
 
 
-//menerima data dari html form
-$berat = validasi($_GET["beratbadan"]);	
-$tinggi = validasi($_GET["tinggibadan"] / 100);
+//menerima dan menetapkan varibale
+$BB = validasi($_GET["beratbadan"]);
+$TB = validasi($_GET["tinggibadan"]);
 
 
-//Cek angka atau tidak
-if (filter_var($berat, FILTER_VALIDATE_INT) or filter_var($berat,FILTER_VALIDATE_FLOAT)){
-	if (filter_var($tinggi, FILTER_VALIDATE_INT)) {
-		$A = nilaiBMI($berat, $tinggi);
-		$B = LabelBMI($BMI);
-		kirimdata($A, $B);
+//cek integer dan float
+if (filter_var($BB, FILTER_VALIDATE_INT) or filter_var($BB,FILTER_VALIDATE_FLOAT)){
+	if (filter_var($TB, FILTER_VALIDATE_INT)) {
+		$nilaibmi = nilaibmi($BB, $TB);
+		$statusbmi = statusbmi($nilaibmi);
+		kirimdata($nilaibmi, $statusbmi);
 	}
-	elseif (filter_var($tinggi, FILTER_VALIDATE_FLOAT)){
-		$A = nilaiBMI($berat, $tinggi);
-		$B = LabelBMI($BMI);
-		kirimdata($A, $B);
+	elseif (filter_var($TB, FILTER_VALIDATE_FLOAT)){
+		$nilaibmi = nilaibmi($BB, $TB);
+		$statusbmi = statusbmi($nilaibmi);
+		kirimdata($nilaibmi, $statusbmi);
 	} else {
 		echo "Input ditolak, jangan nakal :)";
 	}
 } else {
-	echo(" Input ditolak, jangan nakal :)");
+		echo("Input ditolak, jangan nakal :)");
+		
 }
-
-
-//Tutup koneksi
-mysqli_close($conn);
 
 
 ?>
